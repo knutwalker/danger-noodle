a6c7ff7f491933a4bc44f5c5cb821337d3a46007
Paul Horn <developer@knutwalker.de>
2020-11-05 19:09:04 +0100

    Empty bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 #![warn(clippy::complexity)]
 
-fn main() {}
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dComponents::default());
+}
+
+fn main() {
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
+}
