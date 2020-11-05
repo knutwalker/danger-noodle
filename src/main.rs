715c629a2bed5dfeab45037874640e52fd6a44c2
Paul Horn <developer@knutwalker.de>
2020-11-05 19:46:00 +0100

    Move danger noodle on grid

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 use bevy::prelude::*;
 
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+
 fn main() {
     App::build()
         .add_startup_system(set_title.system())
@@ -9,6 +12,8 @@ fn main() {
         .add_startup_stage("game_setup")
         .add_startup_system_to_stage("game_setup", game_setup.system())
         .add_system(danger_noodle_movement.system())
+        .add_system(position_translation.system())
+        .add_system(size_scaling.system())
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -32,29 +37,77 @@ fn game_setup(mut commands: Commands, materials: Res<Materials>) {
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .with(DangerNoodleHead);
+        .with(DangerNoodleHead)
+        .with(Position { x: 3, y: 3 })
+        .with(Size::square(0.8));
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
+    for (size, mut sprite) in q.iter_mut() {
+        let window = windows.get_primary().unwrap();
+        sprite.size = Vec2::new(
+            size.width as f32 / ARENA_WIDTH as f32 * window.width() as f32,
+            size.height as f32 / ARENA_HEIGHT as f32 * window.height() as f32,
+        )
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(p: f32, bound_window: f32, bound_game: f32) -> f32 {
+        p / bound_game * bound_window - (bound_window / 2.0) + (bound_window / bound_game / 2.0)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        )
+    }
 }
 
 fn danger_noodle_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<(&DangerNoodleHead, &mut Transform)>,
+    mut head_positions: Query<(&DangerNoodleHead, &mut Position)>,
 ) {
-    for (_head, mut transform) in head_positions.iter_mut() {
+    for (_head, mut pos) in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            *transform.translation.x_mut() -= 2.;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            *transform.translation.x_mut() += 2.;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            *transform.translation.y_mut() -= 2.;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            *transform.translation.y_mut() += 2.;
+            pos.y += 1;
         }
     }
 }
+
 struct DangerNoodleHead;
 struct Materials {
     head_material: Handle<ColorMaterial>,
+}
+
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
 }
